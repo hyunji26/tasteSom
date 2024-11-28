8a7a7521053a33c828fb170f5875f8b84f895a76
hyunji26 <dlaguswlsl@naver.com>
2024-11-29 00:25:52 +0900

    Add files via upload

---
+++ Dto/MenuDto.java
@@ -0,0 +1,43 @@
+package Dto;
+
+public class MenuDto {
+	private int menuId;
+	private int restaurantId;
+	private int ownerId;
+	private String name;
+	private String price;
+	
+	public MenuDto() {
+		
+	}
+	public int getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(int menuId) {
+		this.menuId = menuId;
+	}
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+	public int getOwnerId() {
+		return ownerId;
+	}
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+}
---
+++ Dto/RestaurantDto.java
@@ -0,0 +1,55 @@
+package Dto;
+
+public class RestaurantDto {
+	private int restaurantId;
+	private int ownerId;
+	private String name;
+	private String location;
+	private float rating;
+	
+	public RestaurantDto() {
+		
+	}
+
+	public int getRestaurantId() {
+		return restaurantId;
+	}
+
+	public void setRestaurantId(int restaurantId) {
+		this.restaurantId = restaurantId;
+	}
+
+	public int getOwnerId() {
+		return ownerId;
+	}
+
+	public void setOwnerId(int ownerId) {
+		this.ownerId = ownerId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public float getRating() {
+		return rating;
+	}
+
+	public void setRating(float rating) {
+		this.rating = rating;
+	}
+	
+
+}
